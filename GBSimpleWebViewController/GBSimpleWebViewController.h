7a9b3f77d7b620c23a7bcfcaeced6eb5426095cb
Luka Mirosevic <luka@goonbee.com>
2013-08-12 15:30:05 +0200

    Initial Commit

---
+++ GBSimpleWebViewController/GBSimpleWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBSimpleWebViewController.h
+//  GBSimpleWebViewController
+//
+//  Created by Luka Mirosevic on 12/08/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBSimpleWebViewController : NSObject
+
+@end
---
+++ GBSimpleWebViewController/GBSimpleWebViewController.m
@@ -0,0 +1,13 @@
+//
+//  GBSimpleWebViewController.m
+//  GBSimpleWebViewController
+//
+//  Created by Luka Mirosevic on 12/08/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBSimpleWebViewController.h"
+
+@implementation GBSimpleWebViewController
+
+@end
